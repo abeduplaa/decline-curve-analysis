5533067840516c28ae391c4b8a5b8a9320324ca0
Randy Zwitch <randy.zwitch@omnisci.com>
2020-02-24 14:32:14 +0000

    changed UDF include to include the header provided by OmniSci

--- udf/udf_penn.cpp
+++ udf/udf_penn.cpp
@@ -1,9 +1,6 @@
 #include <cstdint>
-#include <cmath>
-#if defined(clang) && defined(CUDA) && defined(CUDA_ARCH) #define DEVICE device #else #define DEVICE #endif
-#if defined(clang) && defined(CUDA) && defined(CUDA_ARCH) #define NEVER_INLINE #else #define NEVER_INLINE attribute((noinline)) #endif
-#if defined(clang) && defined(CUDA) && defined(CUDA_ARCH) #define ALWAYS_INLINE #else #define ALWAYS_INLINE attribute((always_inline)) #endif
-#define EXTENSION_NOINLINE extern "C" NEVER_INLINE DEVICE
+#include "/home/mapdadmin/abraham/oil-gas/decline-curve-analysis-gpu/udf/OmniSciTypes.h"
+
 EXTENSION_NOINLINE float udf_penn(const float qi, const float b, const float d, const float t, const int32_t lim) { 
     if (t <= lim){
         return qi / std::pow( (1 + b * d * t, 1/ b) );
