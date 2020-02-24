7ca64ae503bfe8a5681c7994938932274b04db0d
Randy Zwitch <randy.zwitch@omnisci.com>
2020-02-24 13:20:45 +0000

    added a template file to debug and test

---
+++ udf/udf_diff.cpp
@@ -0,0 +1,5 @@
+#include <cstdint>
+#include "/home/mapdadmin/abraham/oil-gas/decline-curve-analysis-gpu/udf/OmniSciTypes.h"
+
+EXTENSION_NOINLINE int32_t udf_diff(const int32_t x, const int32_t y) { return x - y; }
+
